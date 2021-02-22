be65c99c9e066051c171b8eee7669233081af853
Nicolas Frankel <nicolas@frankel.ch>
2021-02-22 22:09:48 +0100

    Move the model structures into their own dedicated module

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
 fn main() {
     println!("Hello, world!");
 }
-
-pub struct Super {
-    pub super_name: String,
-    pub real_name: String,
-    pub power: u16,
-}
-
-pub struct Group {
-    pub name: String,
-    pub members: Vec<Super>,
-}
---
+++ src/model.rs
@@ -0,0 +1,10 @@
+pub struct Super {
+    pub super_name: String,
+    pub real_name: String,
+    pub power: u16,
+}
+
+pub struct Group {
+    pub name: String,
+    pub members: Vec<Super>,
+}
