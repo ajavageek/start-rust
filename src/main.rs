a07be2f07556bfd5322f4a27d3f10fa46ea51f0f
Nicolas Frankel <nicolas@frankel.ch>
2021-02-22 21:11:31 +0100

    Start of model implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
 }
+
+pub struct Super {
+    pub super_name: String,
+    pub real_name: String,
+    pub power: u16,
+}
+
+pub struct Group {
+    pub name: String,
+    pub members: Vec<Super>,
+}
