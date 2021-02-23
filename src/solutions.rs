35287b6aefa2de1d5923da439b6e0a871b465930
Nicolas Frankel <nicolas@frankel.ch>
2021-02-23 02:38:53 +0100

    Implement the first function in the i module

--- src/solutions.rs
+++ src/solutions.rs
@@ -1,1 +1,9 @@
+pub mod i {
+    use crate::model::Group;
 
+    pub fn find_largest_group(groups: &Vec<Group>) -> Option<&Group> {
+        groups
+            .iter()
+            .max_by(|&g1, &g2| g1.members.len().partial_cmp(&g2.members.len()).unwrap())
+    }
+}
