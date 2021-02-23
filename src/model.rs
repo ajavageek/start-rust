e7fb66e46c8051e581a206ff37e63f71813c95a8
Nicolas Frankel <nicolas@frankel.ch>
2021-02-23 03:37:44 +0100

    Implement a more involved test
      Check the function implementation works when the vector collection contains only a single element

--- src/model.rs
+++ src/model.rs
@@ -1,9 +1,11 @@
+#[derive(Debug, PartialEq)]
 pub struct Super {
     pub super_name: String,
     pub real_name: String,
     pub power: u16,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Group {
     pub name: String,
     pub members: Vec<Super>,
--- src/tests/i.rs
+++ src/tests/i.rs
@@ -8,3 +8,15 @@ fn should_return_none_if_groups_is_empty() {
     let result = i::find_largest_group(&groups);
     assert!(result.is_none());
 }
+
+#[test]
+fn should_return_group_if_groups_has_only_one_element() {
+    let group = Group {
+        name: String::from("The Misfits of Science"),
+        members: Vec::new(),
+    };
+    let groups = vec![group];
+    let result = i::find_largest_group(&groups);
+    assert!(result.is_some());
+    assert_eq!(result, groups.first());
+}
